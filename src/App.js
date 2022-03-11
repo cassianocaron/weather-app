522be4a3f6ca5d59a69e754cd0cd6604f8531a4d
Cassiano Caron <cassianocaron@hotmail.com>
2022-03-11 11:46:51 -0300

    Add hourly forecast

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {
   ComboboxOption,
 } from "@reach/combobox";
 import "@reach/combobox/styles.css";
+import { add } from "date-fns";
 
 const libraries = ["places"];
 
@@ -120,6 +121,11 @@ const ShowWeather = ({ weather, location }) => {
         <div className="feels-like">
           Feels like {Math.round(weather.current.feels_like)}°C
         </div>
+        <div className="hourly-forecast">
+          {[...Array(6).keys()].map((i) => (
+            <HourlyForecast index={i} weather={weather} />
+          ))}
+        </div>
       </div>
     </main>
   );
@@ -149,6 +155,35 @@ const GetTime = ({ timezone }) => {
           minute: "2-digit",
           second: "2-digit",
         }).format(dateState)}
+      </div>
+    </div>
+  );
+};
+
+const HourlyForecast = ({ index, weather }) => {
+  const addHour = (hour) => {
+    const date = add(new Date(), { hours: hour });
+    return new Intl.DateTimeFormat("en-US", {
+      timeZone: weather.timezone,
+      hour: "2-digit",
+    }).format(date);
+  };
+
+  return (
+    <div className="hour-forecast">
+      <div>{addHour(index + 1)}</div>
+      <div className="weather-icon-hour">
+        <img
+          alt="weather icon"
+          src={`http://openweathermap.org/img/wn/${weather.hourly[index].weather[0].icon}@2x.png`}
+        />
+      </div>
+      <div>{Math.round(weather.hourly[index].temp)}°</div>
+      <div className="pop">
+        <div className="rain-icon">
+          <img alt="rain icon" src="/drop.svg" />
+        </div>
+        <div>{Math.round(weather.hourly[index].pop * 100)}%</div>
       </div>
     </div>
   );
